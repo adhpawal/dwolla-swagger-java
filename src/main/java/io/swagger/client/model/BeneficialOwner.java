27d40132c151796e14856f1a06d0c6ae8df74540
Pragya Shrestha <pragyashrestha@lftechnology.com>
2018-05-14 19:19:27 +0545

    Add missing properties

--- src/main/java/io/swagger/client/model/BeneficialOwner.java
+++ src/main/java/io/swagger/client/model/BeneficialOwner.java
@@ -5,9 +5,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 @ApiModel(description = "")
 public class BeneficialOwner {
 
+    private Map<String, HalLink> links = new HashMap<String, HalLink>() ;
+    private Object embedded = null;
+    private String id = null;
     private String firstName = null;
     private String lastName = null;
     private String title = null;
@@ -15,7 +22,42 @@ public class BeneficialOwner {
     private String dateOfBirth = null;
     private Address address = null;
     private Passport passport = null;
+    private String status = null;
+    private String locationHeader;
 
+    /**
+     **/
+    @ApiModelProperty( value = "")
+    @JsonProperty("_links")
+    public Map<String, HalLink> getLinks() {
+        return links;
+    }
+    public void setLinks(Map<String, HalLink> links) {
+        this.links = links;
+    }
+
+
+    /**
+     **/
+    @ApiModelProperty(value = "")
+    @JsonProperty("_embedded")
+    public Object getEmbedded() {
+        return embedded;
+    }
+    public void setEmbedded(Object embedded) {
+        this.embedded = embedded;
+    }
+
+    /**
+     **/
+    @ApiModelProperty(required = true, value = "")
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
     /**
      **/
     @ApiModelProperty(required = true, value = "")
@@ -93,10 +135,34 @@ public class BeneficialOwner {
         this.passport = passport;
     }
 
+    /**
+     **/
+    @ApiModelProperty(required = true, value = "")
+    @JsonProperty("status")
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * Used to deserialize Location field in
+     * HTTP headers, primarily for HAL-styled
+     * POST requests.
+     **/
+    @JsonProperty("Location")
+    public String getLocationHeader() { return locationHeader; }
+    public void setLocationHeader(ArrayList<String> locationHeader) { this.locationHeader = locationHeader.get(0); }
+
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("{\n");
+        sb.append("  links: ").append(links).append("\n");
+        sb.append("  embedded: ").append(embedded).append("\n");
+        sb.append("  id: ").append(id).append("\n");
         sb.append("  firstName: ").append(firstName).append("\n");
         sb.append("  lastName: ").append(lastName).append("\n");
         sb.append("  title: ").append(title).append("\n");
@@ -104,6 +170,7 @@ public class BeneficialOwner {
         sb.append("  dateOfBirth: ").append(dateOfBirth).append("\n");
         sb.append("  address: ").append(address).append("\n");
         sb.append("  passport: ").append(passport).append("\n");
+        sb.append("  status: ").append(status).append("\n");
         sb.append("}\n");
         return sb.toString();
     }
